9dffdc42eca4d73db55d920a20a856885ee0f387
gm525619 <105180791+gm525619@users.noreply.github.com>
2022-05-14 13:59:35 -0400

    Adding C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Gavin Morgan
+ *        Date: 5/12/2022
+ * Description: Greetings to Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
